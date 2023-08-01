50e5fda995bbadcb475cab27a83f9b398a0b2ed2
dancefootball <141083250+dancefootball@users.noreply.github.com>
2023-08-01 02:11:18 +0200

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var news = [
+        {
+            title: "Inauguración de la nueva temporada",
+            img: "https://www.example.com/image1.jpg",
+            description: "La nueva temporada de Dance Football se inaugurará la próxima semana, con equipos de todo el mundo compitiendo por la supremacía en este emocionante deporte."
+        },
+        // Añade aquí las demás noticias
+        // ...
+    ];
+
+    var newsCarousel = document.getElementById('news-carousel').querySelector('.carousel-inner');
+    for (var i = 0; i < news.length; i++) {
+        var newsItem = document.createElement('div');
+        newsItem.classList.add('carousel-item');
+        if (i === 0) {
+            newsItem.classList.add('active');
+        }
+        newsItem.innerHTML = `
+            <img src="${news[i].img}" class="d-block w-100" alt="${news[i].title}">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>${news[i].title}</h5>
+                <p>${news[i].description}</p>
+            </div>
+        `;
+        newsCarousel.appendChild(newsItem);
+    }
+});
